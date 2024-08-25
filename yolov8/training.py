46015d45c21774c374d436a8a6ca5f15877a7da3
David Mihola <david.mihola@sperling.cz>
2024-08-25 22:19:30 +0200

    main-Quantization aware training

--- yolov8/training.py
+++ yolov8/training.py
@@ -1,5 +1,8 @@
 from ultralytics import YOLO
+import torch.quantization as quant
 
 model = YOLO("models/yolov8m.pt")                        # load a pretrained model
+model.qconfig = quant.get_default_qat_qconfig('fbgemm')
+model = quant.prepare_qat(model)
 model.train(data="datasets/plates/data.yaml", epochs=3)  # train the model
 metrics = model.val()                                    # evaluate model performance on the validation set
