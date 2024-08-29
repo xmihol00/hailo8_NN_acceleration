3dd00eefc3a6e5ce0c39b584f1eb90ecf0233f66
David Mihola <david.mihola@sperling.cz>
2024-08-29 20:12:38 +0200

    main-Updated training script for YOLOv8

--- yolov8/training.py
+++ yolov8/training.py
@@ -1,8 +1,11 @@
 from ultralytics import YOLO
 import torch.quantization as quant
+import torch
 
-model = YOLO("models/yolov8m.pt")                        # load a pretrained model
-model.qconfig = quant.get_default_qat_qconfig('fbgemm')
-model = quant.prepare_qat(model)
-model.train(data="datasets/plates/data.yaml", epochs=3)  # train the model
-metrics = model.val()                                    # evaluate model performance on the validation set
+model = YOLO("models/yolov8m.pt")                                                  # load a pretrained model
+model.qconfig = quant.get_default_qat_qconfig('fbgemm')                            # set the quantization configuration
+model = quant.prepare_qat(model, inplace=True)                                     # prepare the model for quantization
+results = model.train(data="datasets/plates/data.yaml", epochs=1, int8=True)       # train the model
+model = quant.convert(model, inplace=True)                                         # convert the model to a quantized model
+metrics = model.val()                                                              # evaluate model performance on the validation set
+torch.jit.save(torch.jit.script(model), 'quantized_model.pth')                     # save the quantized model
